30162602a4e07e1caef361a3d7fa9e1261924080
Ruben Verborgh <ruben@verborgh.org>
2018-09-23 22:48:29 +0200

    Accept string expression in DataField.

--- test/components/DataField-test.jsx
+++ test/components/DataField-test.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { DataField } from '../../src/';
 import { shallow } from 'enzyme';
+import * as ldflex from '@solid/query-ldflex';
+
+jest.setMock('@solid/query-ldflex', {});
 
 describe('A DataField', () => {
   describe('without expression', () => {
@@ -14,6 +17,11 @@ describe('A DataField', () => {
       expect(field.text()).toBe('');
     });
 
+    it('has the error message in the error property', () => {
+      expect(field.prop('error'))
+        .toBe('Expected data to be a path or a string but got undefined');
+    });
+
     it('has the solid class', () => {
       expect(field.hasClass('solid')).toBe(true);
     });
@@ -22,12 +30,12 @@ describe('A DataField', () => {
       expect(field.hasClass('data')).toBe(true);
     });
 
-    it('has the empty class', () => {
-      expect(field.hasClass('empty')).toBe(true);
+    it('has the error class', () => {
+      expect(field.hasClass('error')).toBe(true);
     });
   });
 
-  describe('with a data property', () => {
+  describe('with data set to a thenable', () => {
     let field, expression;
     beforeEach(() => {
       expression = { then: jest.fn() };
@@ -119,6 +127,134 @@ describe('A DataField', () => {
           expect(field.text()).toBe('new contents');
         });
       });
+    });
+  });
+
+  describe('with data set to an expression string', () => {
+    let field;
+    beforeEach(() => {
+      ldflex.user = { firstName: { then: jest.fn() } };
+      field = shallow(<DataField data="user.firstName"/>);
+    });
+
+    describe('before the expression resolves', () => {
+      it('is an empty span', () => {
+        expect(field.name()).toBe('span');
+        expect(field.text()).toBe('');
+      });
+
+      it('has the solid class', () => {
+        expect(field.hasClass('solid')).toBe(true);
+      });
+
+      it('has the data class', () => {
+        expect(field.hasClass('data')).toBe(true);
+      });
+
+      it('has the empty class', () => {
+        expect(field.hasClass('empty')).toBe(true);
+      });
+
+      it('starts resolving the expression', () => {
+        expect(ldflex.user.firstName.then).toBeCalledTimes(1);
+      });
+    });
+
+    describe('after the expression resolves', () => {
+      beforeEach(() => {
+        const resolve = ldflex.user.firstName.then.mock.calls[0][0];
+        resolve({ toString: () => 'First' });
+      });
+
+      it('resolves to the evaluated expression', () => {
+        expect(field.text()).toBe('First');
+      });
+    });
+
+    describe('after the expression errors', () => {
+      beforeEach(() => {
+        const reject = ldflex.user.firstName.then.mock.calls[0][1];
+        reject(new Error('the error message'));
+      });
+
+      it('is an empty span', () => {
+        expect(field.name()).toBe('span');
+        expect(field.text()).toBe('');
+      });
+
+      it('has the error message in the error property', () => {
+        expect(field.prop('error')).toBe('the error message');
+      });
+
+      it('has the solid class', () => {
+        expect(field.hasClass('solid')).toBe(true);
+      });
+
+      it('has the data class', () => {
+        expect(field.hasClass('data')).toBe(true);
+      });
+
+      it('has the error class', () => {
+        expect(field.hasClass('error')).toBe(true);
+      });
+    });
+  });
+
+  describe('with data set to an invalid expression string', () => {
+    let field;
+    beforeEach(() => {
+      field = shallow(<DataField data=".invalid"/>);
+    });
+
+    it('is an empty span', () => {
+      expect(field.name()).toBe('span');
+      expect(field.text()).toBe('');
+    });
+
+    it('has the error message in the error property', () => {
+      expect(field.prop('error'))
+        .toBe('Expression ".invalid" is invalid: Unexpected token .');
+    });
+
+    it('has the solid class', () => {
+      expect(field.hasClass('solid')).toBe(true);
+    });
+
+    it('has the data class', () => {
+      expect(field.hasClass('data')).toBe(true);
+    });
+
+    it('has the error class', () => {
+      expect(field.hasClass('error')).toBe(true);
+    });
+  });
+
+  describe('with data set to a non-thenable', () => {
+    let field;
+    beforeEach(() => {
+      field = shallow(<DataField data={{}}/>);
+    });
+
+    it('is an empty span', () => {
+      expect(field.name()).toBe('span');
+      expect(field.text()).toBe('');
+    });
+
+    it('has the error message in the error property', () => {
+      expect(field.prop('error'))
+        .toBe('Expected data to be a path or a string but got [object Object]');
+    });
+
+    it('has the solid class', () => {
+      expect(field.hasClass('solid')).toBe(true);
+    });
+
+    it('has the data class', () => {
+      expect(field.hasClass('data')).toBe(true);
+    });
+
+    it('has the error class', () => {
+      expect(field.hasClass('error')).toBe(true);
     });
   });
 });
