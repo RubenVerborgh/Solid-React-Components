7607fffa2fc15d0c65d877b619833c7e55c9cfac
Ruben Verborgh <ruben@verborgh.org>
2018-10-28 22:43:47 +0100

    Add display name to demo app.

--- demo/app.jsx
+++ demo/app.jsx
@@ -4,28 +4,31 @@ import {
   Value, Image, List, Link, Label,
 } from '../src/';
 
-export default () =>
-  <div>
-    <header>
-      <h1>Solid App</h1>
-      <AuthButton popup="popup.html"/>
-    </header>
-    <main>
-      <LoggedIn>
-        <Image src="user.image" defaultSrc="profile.svg" className="profile"/>
-        <p>Welcome back, <Value src="user.name"/>.</p>
-        <h2>Friends</h2>
-        <List src="user.friends.firstName"/>
-      </LoggedIn>
-      <LoggedOut>
-        <p>You are logged out.</p>
-      </LoggedOut>
-    </main>
-    <footer>
-      <p>
-        Solid React demo app
-        by <Label src="[https://ruben.verborgh.org/profile/#me]"/> {' '}
-        (<Link href="[https://ruben.verborgh.org/profile/#me].homepage"/>)
-      </p>
-    </footer>
-  </div>;
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h1>Solid App</h1>
+        <AuthButton popup="popup.html"/>
+      </header>
+      <main>
+        <LoggedIn>
+          <Image src="user.image" defaultSrc="profile.svg" className="profile"/>
+          <p>Welcome back, <Value src="user.name"/>.</p>
+          <h2>Friends</h2>
+          <List src="user.friends.firstName"/>
+        </LoggedIn>
+        <LoggedOut>
+          <p>You are logged out.</p>
+        </LoggedOut>
+      </main>
+      <footer>
+        <p>
+          Solid React demo app
+          by <Label src="[https://ruben.verborgh.org/profile/#me]"/> {' '}
+          (<Link href="[https://ruben.verborgh.org/profile/#me].homepage"/>)
+        </p>
+      </footer>
+    </div>
+  );
+}
