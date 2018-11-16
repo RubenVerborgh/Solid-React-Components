83e337bb1aee1a2acf18cda06c197f5605f38a82
Angelo Veltens <angelo.veltens@codecentric.de>
2018-11-16 15:30:44 +0100

    override default class names with customization

--- test/components/AuthButton-test.jsx
+++ test/components/AuthButton-test.jsx
@@ -4,31 +4,55 @@ import { mount } from 'enzyme';
 import auth from 'solid-auth-client';
 
 describe('An AuthButton', () => {
-  let button;
-  beforeAll(() => (button = mount(<AuthButton className="custom styling"/>)));
-  afterAll(() => button.unmount());
+  describe('default', function () {
+    let button;
+    beforeEach(() => (button = mount(<AuthButton />)));
+    afterEach(() => button.unmount());
 
-  describe('when the user is not logged in', () => {
-    beforeAll(() => auth.mockWebId(null));
+    describe('when the user is not logged in', () => {
+      beforeAll(() => auth.mockWebId(null));
 
-    it('renders the log in button', () => {
-      expect(button.text()).toBe('Log in');
+      it('renders the log in button', () => {
+        expect(button.text()).toBe('Log in');
+      });
+
+      it('uses default class names', () => {
+        expect(button.find('button').prop('className')).toEqual('solid auth login');
+      });
     });
 
-    it('respects custom styling in button class name', () => {
-      expect(button.find('button').prop('className')).toEqual('solid auth login custom styling');
+    describe('renders the log out button', () => {
+      beforeAll(() => auth.mockWebId('https://example.org/#me'));
+
+      it('has "Log out" as text', () => {
+        expect(button.text()).toBe('Log out');
+      });
+
+      it('uses default class names', () => {
+        expect(button.find('button').prop('className')).toEqual('solid auth logout');
+      });
     });
   });
 
-  describe('renders the log out button', () => {
-    beforeAll(() => auth.mockWebId('https://example.org/#me'));
+  describe('custom class names', function () {
+    let button;
+    beforeEach(() => (button = mount(<AuthButton className="custom styling"/>)));
+    afterEach(() => button.unmount());
 
-    it('has "Log out" as text', () => {
-      expect(button.text()).toBe('Log out');
+    describe('when the user is not logged in', () => {
+      beforeAll(() => auth.mockWebId(null));
+
+      it('respects custom styling in button class name', () => {
+        expect(button.find('button').prop('className')).toEqual('custom styling');
+      });
     });
 
-    it('respects custom styling in button class name', () => {
-      expect(button.find('button').prop('className')).toEqual('solid auth login custom styling');
+    describe('renders the log out button', () => {
+      beforeAll(() => auth.mockWebId('https://example.org/#me'));
+
+      it('respects custom styling in button class name', () => {
+        expect(button.find('button').prop('className')).toEqual('custom styling');
+      });
     });
   });
 });
