e3c0c8e79c2571f3052c85830e2e8711ff19a87a
Ruben Verborgh <ruben@verborgh.org>
2019-03-20 13:24:53 -0400

    Exclude React from library build.

--- webpack/webpack.lib.config.js
+++ webpack/webpack.lib.config.js
@@ -1,16 +1,23 @@
 /*
   Exports only @solid/react itself, expecting:
+  - global.React to be react
+  - global.PropTypes to be prop-types
   - global.solid.auth to be solid-auth-client
   - global.solid.data to be @solid/query-ldflex
 */
 
 const extendConfig = require('./webpack.common.config');
 
-module.exports = extendConfig('./dist', (outputDir, common) => ({
+module.exports = extendConfig('./dist', (outputDir, { externals, ...common }) => ({
   ...common,
   output: {
     path: outputDir,
     filename: '[name].js',
     library: ['solid', 'react'],
+  },
+  externals: {
+    ...externals,
+    'react': 'React',
+    'prop-types': 'PropTypes',
   },
 }));
